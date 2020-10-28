6d9bb35172dbf328174e5b839f306969e5002998
acidb <acidb@10.0.0.31>
2020-10-27 20:37:58 -0400

    Revert "This is a project for capstone"
    This reverts commit cfa671fe21dd6b514f642375dc169f14b99eb4cb.

--- src/main/java/project/chapter4.java
+++ src/main/java/project/chapter4.java
@@ -29,7 +29,7 @@ public class chapter4 extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("ISO 17025 Chapter 4");
         setLocation(new java.awt.Point(500, 200));
 
